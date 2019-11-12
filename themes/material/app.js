25a2a52d86707af8fce40f02ef5dd94acb241eca
aaaaTI <30612014+ATIAboo@users.noreply.github.com>
2019-11-12 17:33:29 +0800

    Update app.js

--- themes/material/app.js
+++ themes/material/app.js
@@ -6,7 +6,7 @@ document.write('<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdu
 // 初始化页面，并载入必要资源
 function init(){
     document.siteName = $('title').html();
-    $('body').addClass("mdui-theme-primary-orange mdui-color-purple-accent ");
+    $('body').addClass("mdui-theme-primary-orange mdui-color-accent-blue ");
     var html = `
 <header class="mdui-appbar mdui-color-theme"> 
    <div id="nav" class="mdui-toolbar mdui-container"> 
